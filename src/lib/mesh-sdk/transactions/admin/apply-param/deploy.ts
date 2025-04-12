e6ac44b998ffc1641d144c1c7aecbf18f3bdc6eb
Emmanuel .A <asaoluemma@gmail.com>
2025-04-12 21:41:34 +0100

    update plutus-script export to apply params

---
+++ src/lib/mesh-sdk/transactions/admin/apply-param/deploy.ts
@@ -0,0 +1,26 @@
+
+import { applyParamsToScript } from "@meshsdk/core-cst";
+import plutusBlueprint from "../../../onchain/src/plutus.json" with {type: 'json'}; 
+import { PlutusScript } from "@meshsdk/core";
+
+const asteriaValidator = plutusBlueprint.validators.find(
+        ({ title }) => title === "deploy.deploy.spend"
+  );
+
+const DEPLOY_SCRIPT = asteriaValidator!.compiledCode;
+
+function deployScriptAppliedParam( admin_token:any){
+    const appliedDeployParam   = applyParamsToScript(
+      DEPLOY_SCRIPT,
+      [admin_token],
+      "JSON"
+    );
+
+  const deployPlutusScript : PlutusScript = {
+    code: appliedDeployParam,
+    version: "V3"
+  };
+return {deployPlutusScript,appliedDeployParam};
+};
+
+export {deployScriptAppliedParam};
---
+++ src/lib/mesh-sdk/transactions/admin/apply-param/pellet.ts
@@ -0,0 +1,27 @@
+
+import { applyParamsToScript } from "@meshsdk/core-cst";
+import plutusBlueprint from "../../../onchain/src/plutus.json" with {type: 'json'};
+import { PlutusScript } from "@meshsdk/core";
+    
+const pelletValidator = plutusBlueprint.validators.find(
+    ({ title }) => title === "pellet.pellet.spend"
+  );
+        
+const PELLET_SCRIPT = pelletValidator!.compiledCode;
+
+function  pelletScriptApliedParam(admin_token:any){
+
+  const appliedPelletParam   = applyParamsToScript(
+    PELLET_SCRIPT,
+    [admin_token],
+    "JSON",
+    );
+
+  const pelletPlutusScript: PlutusScript = {
+    code: appliedPelletParam,
+    version: "V3"
+    };
+return {pelletPlutusScript, appliedPelletParam};
+};
+                      
+export {pelletScriptApliedParam};
