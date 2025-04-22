cf83e8571d4a4e3d82ea37cb27ef8a1fb3d46004
Newman S Lanier <nslanier@yahoo.com>
2025-04-22 14:11:29 -0400

    Update config.ts with current adminToken

--- config.ts
+++ config.ts
@@ -4,9 +4,8 @@ import { integer, policyId, assetName, scriptHash, scriptAddress } from "@meshsd
 // Variables to be used throughout App 
 // use .env for passwords and api keys
 
-
-export const adminTokenName: assetName = '41737465726961746f6b656e'
-export const adminTokenPolicy: policyId = 'fbeafbfb456a440b174001793c546c93bdf887730c1e12b2f9f0d293' //bytestring
+export const adminTokenName: assetName = '41737465726961436f696e'
+export const adminTokenPolicy: policyId = 'f06038e916ab3f1fb15280641280f8c13cfd684f6ce31ac000ed6e8f'
 
 export const fuelTokenName: assetName = '4655454c'
 export const fuelTokenPolicy: policyId = '3830a960240a7461fd8c121d2df596d5efb87fa7670c7285b9ad4000'
