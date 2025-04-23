d1cbf3d27bab3823d05bd20e7f379ddf979a4460
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-04-23 15:35:26 +0200

    Corrected function to check for admin token once wallet is connected, pulling type error but works and  will fix in future commit

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -8,7 +8,7 @@ export default function AdminPage() {
   
   return (
   <div>
-
+    <CardanoWallet />
     
     <h1 className="mb-2">THIS IS Admin</h1>
     <Link className="font-bold " href="/admin/deploy-pellet">Deploy Pellets - click me </Link>
