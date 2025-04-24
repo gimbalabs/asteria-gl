93c851e5f5be1a86336d117c2c4a1de28712aa1c
gZero <gulla0.harsha@gmail.com>
2025-04-24 00:20:11 -0400

    basic structure of asteria site

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -4,13 +4,10 @@ export default function NavBar() {
   return (
     <nav className="w-full bg-black bg-opacity-40 px-6 py-4 shadow-md text-white">
       <div className="container mx-auto flex justify-between items-center">
-        <div className="text-lg font-bold">Asteria</div>
+        <div className="text-lg font-bold"><Link href="/">Asteria</Link></div>
         <ul className="flex gap-4 text-sm font-medium">
-          <li><Link href="/">Home</Link></li>
-          <li><Link href="/admin">Admin</Link></li>
-          <li><Link href="/getstarted">Getting Started</Link></li>
-          <li><Link href="/rules">Rules</Link></li>
           <li><Link href="/map">Map</Link></li>
+          <li><Link href="/about">About</Link></li>
         </ul>
       </div>
     </nav>
---
+++ src/pages/about/index.tsx
@@ -0,0 +1,25 @@
+export default function AboutPage() {
+    return (
+      <div className="p-8">
+        <h2>About Page</h2>
+  
+        <h3>Team Members and Credits</h3>
+        <ul className="list-disc list-inside">
+          <li>t</li>
+          <li>g</li>
+          <li>ne</li>
+          <li>na</li>
+        </ul>
+  
+        <h4>Mentors</h4>
+        <ul className="list-disc list-inside">
+          <li>James</li>
+          <li>Adrian</li>
+          <li>Prof. Mix</li>
+        </ul>
+  
+        <p>Some details about the project</p>
+      </div>
+    );
+  }
+  
