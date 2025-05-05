a25e2fe6ca64eade659404d0b3e6b2006ebada70
Emmanuel .A <asaoluemma@gmail.com>
2025-05-05 13:10:04 +0100

    adding maestro provider

--- src/lib/mesh-sdk/utils.ts
+++ src/lib/mesh-sdk/utils.ts
@@ -1,24 +1,24 @@
-import { MeshWallet,BlockfrostProvider } from "@meshsdk/core";
-
+import { MeshWallet, BlockfrostProvider, MaestroProvider} from "@meshsdk/core";
 
 //get blockfrost provider api key
-const blockchainProvider = new BlockfrostProvider('');
-const myWallet = new MeshWallet({
+export const blockchainProvider = new BlockfrostProvider('');
+export const myWallet = new MeshWallet({
   networkId: 0, // 0: testnet, 1: mainnet
   fetcher: blockchainProvider,
   submitter: blockchainProvider,
   key: {
     type: 'mnemonic',
-    words:[]
+    words: [],
   },
 });
 
-const admintoken = {
-  policyid: "f06038e916ab3f1fb15280641280f8c13cfd684f6ce31ac000ed6e8f",
-  name:    "41737465726961436f696e",
-}
-const blockData = await blockchainProvider.fetchLatestBlock();
-const latestSlot = blockData.slot;
-const slot = Number(latestSlot) + 300;
+export const maestroprovider = new MaestroProvider({
+  network: "Preprod",
+  apiKey: "",
+  turboSubmit:false
+});
 
-export {myWallet,blockchainProvider,admintoken,slot, latestSlot};
+export const blockData = await blockchainProvider.fetchLatestBlock();
+export const latestSlot = blockData.slot;
+export const tx_latest_posix_time = Number(latestSlot) + 600;
+export const slot = Number(latestSlot);
