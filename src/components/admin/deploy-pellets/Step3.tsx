73baefacd578326ac4e63bc617f0317874bb5345
gZero <gulla0.harsha@gmail.com>
2025-05-20 14:12:20 -0400

    changed some comments

--- src/components/admin/deploy-pellets/Step3.tsx
+++ src/components/admin/deploy-pellets/Step3.tsx
@@ -1,13 +1,10 @@
-// pellet array is in the index of deploy-pellets
-
-//recieve pellet state as a prop, clikc deploy send info to the backend
-// recieved from the front end, pull from database
-//sends unsigned tx to the front, user starts signing 
-//pellet prop passed from parent (index of deploy-pellets)
+// pellet array is in the index of deploy-pellets and is passed as a prop
+//receives unsigned tx and user starts signing 
 //click deploy pellets button, send the pellets to the backend (server/api/routers/admin/pelletDeploy.ts) 
 
 import { PelletParams } from "~/utils/pelletUtils";
 import { api } from "~/utils/api";
+
 export default function Step3({pellets}: {pellets: PelletParams}) {
     const deployPellets = api.pelletDeploy.deploy.useMutation();
 
