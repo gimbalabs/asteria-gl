f968a3db37c627a0f59cb196d0ccaeff712ea897
Newman <nslanier@yahoo.com>
2025-05-20 11:46:28 -0400

    adding planning comments

--- src/components/admin/deploy-pellets/Step3.tsx
+++ src/components/admin/deploy-pellets/Step3.tsx
@@ -1,3 +1,11 @@
+// pellet array is in the index of deploy-pellets
+// create a series of batches of transactions that create UTXOs on the pellet validator
+
+//recieve pellet state as a prop, clikc deploy send info to the backend
+// recieved from the front end, pull from database
+//sends unsigned tx to the front, user starts signing 
+//pellet prop passed from parent (index of deploy-pellets)
+//click deploy pellets button, send the pellets to the backend (server/api/routers/admin/pelletDeploy.ts) 
 
 
 export default function Step3() {
