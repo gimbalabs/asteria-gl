83f2a5cd441873e3e2babd3f30d1ba437e9f3c22
gZero <gulla0.harsha@gmail.com>
2025-06-11 21:32:03 -0400

    figures how to use async functions like getAsset()

--- src/components/user/MoveShipComponent.tsx
+++ src/components/user/MoveShipComponent.tsx
@@ -1,10 +1,33 @@
 import { useWallet } from "@meshsdk/react";
+import { useEffect, useState } from "react";
+import { AssetExtended } from "@meshsdk/core";
 
+export default function MoveShipComponent() {
+    const { wallet, connected } = useWallet();
+    const [assets, setAssets] = useState<AssetExtended[]>([]);
 
-export default function MoveShipComponent(){
+    useEffect(() => {
+        const getAssets = async () => {
+            if (connected && wallet) {
+                const walletAssets = await wallet.getAssets();
+                setAssets(walletAssets);
+                console.log(walletAssets);
+            }
+        };
+        getAssets();
+    }, [wallet, connected]);
+
+    if (!connected || !wallet) {
+        return (
+            <div>
+                <h1>Connect wallet to view your ship</h1>
+            </div>
+        );
+    }
+
     return (
         <div>
             <h1>Move Ship</h1>
         </div>
-    )
+    );
 }
