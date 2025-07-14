70395ab8ec81d655912b3339dffd1a564f17e93c
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-07-14 11:47:48 +0200

    switch bytestring -> assetName serializer deploy.ts

--- src/lib/mesh-sdk/transactions/admin/apply-param/deploy.ts
+++ src/lib/mesh-sdk/transactions/admin/apply-param/deploy.ts
@@ -3,7 +3,7 @@ import { applyParamsToScript } from "@meshsdk/core-cst";
 import plutusBlueprint from "../../../../onchain/src/plutus.json" with {type: 'json'}; 
 import { PlutusScript } from "@meshsdk/core";
 
-import { policyId, byteString, conStr0, stringToHex } from "@meshsdk/core";
+import { policyId, byteString, assetName, conStr0, stringToHex } from "@meshsdk/core";
 
 const asteriaValidator = plutusBlueprint.validators.find(
         ({ title }) => title === "deploy.deploy.spend"
@@ -17,7 +17,7 @@ function deployScriptAppliedParam( admin_token: string, adminTokenName: string){
 
   const AdminTokenData =  conStr0([
     policyId(admin_token), 
-    byteString(stringToHex(adminTokenName))                              
+    assetName(stringToHex(adminTokenName))                              
   ]);
 
   const appliedDeployParam   = applyParamsToScript(
