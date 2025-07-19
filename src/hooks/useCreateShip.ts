f02a4c6263fe7cbf66b95dce9f7ae40a76093775
Emmanuel .A <asaoluemma@gmail.com>
2025-07-20 00:38:23 +0100

    format document

--- src/hooks/useCreateShip.ts
+++ src/hooks/useCreateShip.ts
@@ -3,77 +3,71 @@ import { useWallet } from "@meshsdk/react";
 
 import { useState } from "react";
 
+export function useCreateShipTx() {
+  const prepareTx = api.createShip.prepareCreateShipTx.useMutation();
 
+  const [shipFee, setShipFee] = useState(3000000);
+  const [posX, setPosX] = useState(0);
+  const [posY, setPosY] = useState(0);
+  const [initialFuel, setInitialFuel] = useState("20");
 
-export function useCreateShipTx(){
+  const { wallet, connected } = useWallet();
 
-    const prepareTx = api.createShip.prepareCreateShipTx.useMutation();
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
 
-    const [shipFee,  setShipFee]  = useState(3000000);
-    const [posX, setPosX] = useState(0);
-    const [posY, setPosY] = useState(0);
-    const [initialFuel, setInitialFuel] = useState("20");
+    try {
+      if (!connected || !wallet) {
+        throw new Error("Wallet not connected");
+      }
 
-    const { wallet, connected } = useWallet(); 
+      const utxos = await wallet.getUtxos();
+      const changeAddress = await wallet.getChangeAddress();
+      const collateral = await wallet.getCollateral();
 
+      const payload = {
+        utxos,
+        changeAddress,
+        collateral: collateral[0],
+        ship_mint_lovelace_fee: Number(shipFee),
+        initial_fuel: initialFuel,
+        posX: Number(posX),
+        posY: Number(posY),
+        tx_latest_posix_time: Date.now(),
+      };
 
-    const handleSubmit = async (e: React.FormEvent) => {
-      e.preventDefault();
-    
-      try {
-        if (!connected || !wallet) {
-          throw new Error("Wallet not connected");
-        }
-  
-        const utxos = await wallet.getUtxos();
-        const changeAddress = await wallet.getChangeAddress()  
-        const collateral = await wallet.getCollateral() 
+      const { unsignedTx, error } = await prepareTx.mutateAsync(payload);
+      console.log("unstx", unsignedTx);
 
-        const payload = { 
-          utxos,
-          changeAddress,
-          collateral: collateral[0] ,
-          ship_mint_lovelace_fee: Number(shipFee),
-          initial_fuel: initialFuel,
-          posX: Number(posX),
-          posY: Number(posY),
-          tx_latest_posix_time: Date.now(),
-        }
-        
-  
-        
-        const {unsignedTx, error} = await prepareTx.mutateAsync(payload);
-        console.log("unstx" , unsignedTx)
+      if (error) {
+        console.log(error);
+        alert("Error from router" + error);
+      }
 
-        if(error){
-            console.log(error)
-            alert("Error from router" + error)
-        }
+      console.log(changeAddress);
 
-        console.log(changeAddress)
+      console.log("received unsigned tx");
 
-        console.log("received unsigned tx")
+      const signedTx = await wallet.signTx(unsignedTx!, true);
+      const txHash = await wallet.submitTx(signedTx);
 
-        const signedTx = await wallet.signTx(unsignedTx, true);
-        const txHash = await wallet.submitTx(signedTx);
-
-
-        console.log("Transaction Hash:", txHash);
-        alert("Deployed Successfully! TxHash: " + txHash);
-        
-
-        
-
-        } catch (error) {
-            console.error(error);
-            alert("Tranasction Failed");
-        }
-        
-    
-    
-    
+      console.log("Transaction Hash:", txHash);
+      alert("Deployed Successfully! TxHash: " + txHash);
+    } catch (error) {
+      console.error(error);
+      alert("Tranasction Failed");
     }
+  };
 
-    return {shipFee, setShipFee, posX, setPosX, posY, setPosY, initialFuel, setInitialFuel, handleSubmit}
-
+  return {
+    shipFee,
+    setShipFee,
+    posX,
+    setPosX,
+    posY,
+    setPosY,
+    initialFuel,
+    setInitialFuel,
+    handleSubmit,
+  };
 }
