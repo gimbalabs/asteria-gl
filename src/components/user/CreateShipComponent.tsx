6df83d0e8509cf6f4c2a6369e8a1e824b083a7e8
Emmanuel .A <asaoluemma@gmail.com>
2025-07-20 00:37:53 +0100

    format document

--- src/components/user/CreateShipComponent.tsx
+++ src/components/user/CreateShipComponent.tsx
@@ -1,48 +1,60 @@
 import { useWallet } from "@meshsdk/react";
-import { useCreateShipTx } from "~/hooks/useCreateShip"
+import { useCreateShipTx } from "~/hooks/useCreateShip";
 
+export default function CreateShipComponent() {
+  const {
+    shipFee,
+    setShipFee,
+    posX,
+    setPosX,
+    posY,
+    setPosY,
+    initialFuel,
+    setInitialFuel,
+    handleSubmit,
+  } = useCreateShipTx();
+  const { connected } = useWallet();
 
-export default function createShipComponent(){
-
-    const {shipFee, setShipFee, posX, setPosX, posY, setPosY, initialFuel, setInitialFuel, handleSubmit} = useCreateShipTx()
-    const {connected} = useWallet();
-
-    if (!connected) {
-        return (
-        <h2 className="font-bold text-galaxy-danger">
-            Please connect your wallet before Creating your ship
-        </h2>
-        );
-    }
-
-
+  if (!connected) {
     return (
-        <div>
+      <h2 className="font-bold text-galaxy-danger">
+        Please connect your wallet before Creating your ship
+      </h2>
+    );
+  }
 
-            
-            <form onSubmit={handleSubmit} className="flex flex-col gap-2 items-center text-black">
-                <h2>Choose you coordinates and the Click the button to Create your Asteria Ship</h2>
-                <input 
-                    value={posX}
-                    placeholder="Enter the X Coordinate"
-                    onChange={(e) => {setPosX(Number(e.target.value))}}
-                />
-
-`               <input 
-                    value={posY}
-                    placeholder="Enter the Y Coordinate"
-                    onChange={(e) => {setPosY(Number(e.target.value))}}
-                />
-
-                <button type="submit" className={`inline-block px-6 py-3 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2`}>
-                    Create ship
-                </button>
-
-
-            </form> 
-            
-
-
-        </div>
-    )
+  return (
+    <div>
+      <form
+        onSubmit={handleSubmit}
+        className="flex flex-col items-center gap-2 text-black"
+      >
+        <h2>
+          Choose you coordinates and the Click the button to Create your Asteria
+          Ship
+        </h2>
+        <input
+          value={posX}
+          placeholder="Enter the X Coordinate"
+          onChange={(e) => {
+            setPosX(Number(e.target.value));
+          }}
+        />
+        `{" "}
+        <input
+          value={posY}
+          placeholder="Enter the Y Coordinate"
+          onChange={(e) => {
+            setPosY(Number(e.target.value));
+          }}
+        />
+        <button
+          type="submit"
+          className={`inline-block rounded-lg px-6 py-3 font-semibold text-white shadow-md focus:outline-none focus:ring-2`}
+        >
+          Create ship
+        </button>
+      </form>
+    </div>
+  );
 }
