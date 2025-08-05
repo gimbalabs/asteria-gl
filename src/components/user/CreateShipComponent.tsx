09768e7869d6b26b7571892de26760df4129b4cd
wattsmainsanglais <wattsmainsanglais@gmail.com>
2025-08-05 14:52:33 +0200

    add number randomiser for crerate ship grid references, limited to min asteria distance

--- src/components/user/CreateShipComponent.tsx
+++ src/components/user/CreateShipComponent.tsx
@@ -4,7 +4,7 @@ import { useCreateShipTx } from "~/hooks/useCreateShip"
 
 export default function createShipComponent(){
 
-    const { posX, setPosX, posY, setPosY, handleSubmit} = useCreateShipTx()
+    const {handleSubmit} = useCreateShipTx()
     const {connected} = useWallet();
 
     if (!connected) {
@@ -17,22 +17,11 @@ export default function createShipComponent(){
 
 
     return (
-        <div>
+        <div className="pt-5 pb-5">
 
             
             <form onSubmit={handleSubmit} className="flex flex-col gap-2 items-center text-black">
-                <h2>Choose you coordinates and the Click the button to Create your Asteria Ship</h2>
-                <input 
-                    value={posX}
-                    placeholder="Enter the X Coordinate"
-                    onChange={(e) => {setPosX(Number(e.target.value))}}
-                />
-
-`               <input 
-                    value={posY}
-                    placeholder="Enter the Y Coordinate"
-                    onChange={(e) => {setPosY(Number(e.target.value))}}
-                />
+                <h2 className="text-white">Click the button to create an asteria ship, you'll be randomly placed on the grid </h2>
 
                 <button type="submit" className={`inline-block px-6 py-3 text-white font-semibold rounded-lg shadow-md focus:outline-none focus:ring-2`}>
                     Create ship
